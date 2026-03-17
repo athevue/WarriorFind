722cceffeeece2e4ab746539c566a3b8aad2c1df
Krutika Patil <krutikapatil125@gmail.com>
2026-03-17 19:03:34 -0400

    Signup intialization

---
+++ src/Signup.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import {auth} from './firebase/auth';
+
+export default function SignUp(){
+    const [email,setEmail] = useState("");
+    const [password,setPassword]=useState("");
+}
+
--- src/assets/Signup.jsx
+++
@@ -1,3 +0,0 @@
-import {auth} from 'firebase/auth';
-import {db} from 'firebase/db';
-
