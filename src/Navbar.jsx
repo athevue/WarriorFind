ed5117d27fb7dd008c826f75afe2f1cb990c29e6
Adam Vue <athevue@gmail.com>
2026-04-19 01:14:25 -0400

    Refactored code for responsiveness for the NavBar and Hero Section of the home page

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,26 +1,38 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className="navbar">
+      
       <div className="nav-logo">
         <Link to="/">WarriorFind</Link>
       </div>
 
-      <div className="nav-actions">
-        <Link to="/browse">
+      {/* Hamburger icon */}
+      <div className="hamburger" onClick={() => setOpen(!open)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <div className={`nav-actions ${open ? "active" : ""}`}>
+        <Link to="/browse" onClick={() => setOpen(false)}>
           <button className="nav-text">Browse</button>
         </Link>
-        <Link to="/signin">
+
+        <Link to="/signin" onClick={() => setOpen(false)}>
           <button className="nav-text">Sign In</button>
         </Link>
 
-        <Link to="/signup"> 
+        <Link to="/signup" onClick={() => setOpen(false)}>
           <button className="nav-signup">Sign Up</button>
         </Link>
-        
       </div>
+
     </nav>
   );
 }
